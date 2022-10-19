f5240d3bf8315266759bb40b2f3686f64594170d
Mauricio Alvarez Heredia <mauro.h01@gmail.com>
2022-10-19 14:49:45 -0500

    NavBar ok

---
+++ tienda-warbug/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+//Importamos los componentes creados
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Navbars from './layouts/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Navbars />}>
+            <Route index element={ <Home />}/>
+            <Route path='About' element={ <About />}/>
+            <Route path='Contact' element={ <Contact />}/>
+
+            <Route path='*' element={ <Navigate replace to="/" /> }/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tienda-warbug/src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <div>
+            <h1>View About</h1>
+        </div>
+    )
+}
+
+export default About;
