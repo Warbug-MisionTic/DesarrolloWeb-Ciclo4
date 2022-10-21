97ff299ee9ea7e69f1308ce1493b14e594248fc1
Diego Gaitan <gaitandiego10@gmail.com>
2022-10-21 09:07:48 -0500

    el admin puede ver los productos

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -5,6 +5,7 @@ import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Home from '../pages/Home';
 import Navbars from '../layouts/Navbar';
+import Products from '../pages/Products';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path='about' element={<About />} />
             <Route path='contact' element={<Contact />} />
+            <Route path='products' element={<Products />} />
             <Route path='*' element={<Navigate replace to="/" />} />
           </Route>
         </Routes>
