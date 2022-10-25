bc5c8aa56b928e1235f8b55564738e17ea735869
triviglenda <111029511+triviglenda@users.noreply.github.com>
2022-10-24 19:36:42 -0500

    Modificacion vistas sales

--- src/pages/Admin/sales.jsx
+++ src/pages/Admin/sales.jsx
@@ -1,7 +1,66 @@
-const Sales = () => {
+/* const Sales = () => {
     return (
     <p>Ventas </p>
        );
 }
+ */
 
-export default Sales;
+import { Row, Col, Button, ButtonGroup, Table } from 'react-bootstrap';
+import * as Icon from 'react-feather';
+import React, { useState } from 'react';
+import { compose } from 'recompose';
+import { withRouter } from '../../router/withRouter';
+
+const Sales = (props) => {
+    const [items, setItems] = useState([])
+    return (
+        <div className='container mt-10'>
+            <Row>
+                <Col xl={12}>
+                    <div className="card mb-4">
+                        <div className="card-body">
+                            <div className="card-header">
+                                <h5 className="card-title">Lista de Ventas</h5>
+                            </div>
+
+                            <Table responsive hover bordered className="m-0 text-center" >
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Fecha</th>
+                                        <th>IdVenta</th>
+                                        <th className="text-center">Detalle</th>
+                                        <th className="text-center">Valor</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {items && items.map((item, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td className="text-left">{index + 1}</td>
+                                                <td className="text-left">{item.Fecha}</td>
+                                                <td className="text-left">{item.IdVenta}</td>
+                                                <td className="text-left">{item.Detalle}</td>
+                                                <td className="text-left">{item.Valor}</td>
+                                                <td>
+                                                    <ButtonGroup size="sm" aria-label="Basic example">
+                                                        <Button onClick={() => console.log('holaa')} variant="outline-secondary"><Icon.Edit3 className="icon wh-15" /></Button>
+                                                        <Button variant="outline-secondary" onClick={() => console.log('hola')}><Icon.Trash2 className="icon wh-15" /></Button>
+                                                    </ButtonGroup>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </Table>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default compose(
+    withRouter
+)(Sales);
