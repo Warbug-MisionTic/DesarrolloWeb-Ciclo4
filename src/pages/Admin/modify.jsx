fb9720fd6072e3c675da1ef534efce93840c1d71
juandijez <juandijez@gmail.com>
2022-10-28 11:43:50 -0500

    añadir funcionalidad modificar productos

--- src/pages/Admin/modify.jsx
+++ src/pages/Admin/modify.jsx
@@ -2,31 +2,42 @@ import Sidebars from "../../components/Sidebar";
 import { withRouter } from "../../router/withRouter";
 import { compose } from "recompose";
 import React, {useState} from "react";
+import Form from 'react-bootstrap/Form';
 
 const Modify = (props) => {
-    console.log(props)
-    const [product, setProduct] = useState(props.location.state);
-    console.log(product)
+    const [titulo, setTitulo] = useState(props.location.state.titulo)
+    const [descripcion, setDescripcion] = useState(props.location.state.descripcion)
+    const [precio, setPrecio] = useState(props.location.state.precio)
+    const [ubicar, setUbicar] = useState(require(`../../assets/img/${props.location.state.ubicar}`))
+    const onImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+          setUbicar(URL.createObjectURL(event.target.files[0]));
+        }
+       }
   return (
     <div className="ContenedorModPadre">
-      <Sidebars />
+     
 
       <div className="ContenedorMod">
-        <h3>Productos a modificar </h3>
+        <h3>Modificar Productos </h3>
         
         <div className="contenedor-main">
           <div className="contenedor-product">
-            <img className="imagenProducto" src="https://user-images.githubusercontent.com/106354407/197634271-c22fd33e-8d6b-496c-9ea6-75f29c77a81a.png" />
-            <button className="botoncambiarimg"> Cambiar imagen</button>
+            <img className="imagenProducto" src={ubicar} />
+            <Form.Group controlId="formFile" className="mb-3" onChange={onImageChange}>
+                <Form.Label>Cambiar imagen</Form.Label>
+                <Form.Control type="file" />
+            </Form.Group>
+            
           </div>
 
           <div className="formulario">
             <p className="labelMod">Nombre del producto</p>{" "}
-            <input type="text" className="fieldProduct" onChange={event => setProduct(event.target.product)} /> <br />
+            <input type="text" className="fieldProduct" onChange={event => setTitulo(event.target.value)} value={titulo} /> <br />
             <p className="labelMod">Descripción </p>{" "}
-            <input type="text" className="fieldProduct" /> <br />
+            <input type="text" className="fieldProduct" onChange={event => setDescripcion(event.target.value)} value={descripcion} /> <br />
             <p className="labelMod">Precio</p>{" "}
-            <input type="text" className="fieldProduct" />
+            <input type="text" className="fieldProduct" onChange={event => setPrecio(event.target.value)} value={precio}/>
             <br />
             <button className="botoncambiarimg"> Guardar cambios </button>
           </div>
