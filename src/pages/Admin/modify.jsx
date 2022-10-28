b0c54b73d366e5348f4b6c7d5d7cea3046533a64
juandijez <juandijez@gmail.com>
2022-10-27 21:33:53 -0500

    estilos modificar productos

--- src/pages/Admin/modify.jsx
+++ src/pages/Admin/modify.jsx
@@ -5,28 +5,34 @@ import products from "../../jsons/products.json";
 
 
 
+
 const Modify = () => {
     return (
-        <>
             <div className="ContenedorModPadre">
                 <Sidebars />
+                
                 <div className="ContenedorMod">
                     <h3>Productos a modificar </h3>
-                    <form action="">
-                        <image>   </image>
-                        <button> Cambiar imagen</button>
+                    <div className="contenedor-main">
+                        <div className="contenedor-product">
+                            <img className="imagenProducto" src="#" />
+                            <button className="botoncambiarimg"> Cambiar imagen</button>
+                        </div>
+
                         <div className="formulario">
                             <p className="labelMod">Nombre del producto</p>  <input type="text" className="fieldProduct" /> <br /> 
                             <p className="labelMod">Descripción </p> <input type="text" className="fieldProduct" /> <br />
                             <p className="labelMod">Precio</p> <input type="text" className="fieldProduct"/><br />
                         </div>
-                    <button> Guardar cambios </button>
-                    </form>
-                </div>  
-                <Outlet />   
+                        <button> Guardar cambios </button>
 
+                    </div>
+                   
+                        
+                    
+                </div>  
+                {/* <Outlet /> */}   
             </div>
-        </>
     );
 }
 
