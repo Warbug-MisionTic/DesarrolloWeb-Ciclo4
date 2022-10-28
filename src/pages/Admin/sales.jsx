14a0065f08d0ce6cb093a8de7400d2033ac449f2
triviglenda <111029511+triviglenda@users.noreply.github.com>
2022-10-27 23:33:28 -0500

    Lista de ventas

--- src/pages/Admin/sales.jsx
+++ src/pages/Admin/sales.jsx
@@ -3,57 +3,53 @@ import * as Icon from 'react-feather';
 import React, { useState } from 'react';
 import { compose } from 'recompose';
 import { withRouter } from '../../router/withRouter';
+import productsLista from "../../jsons/products.json";
 
 const Sales = (props) => {
-    const [items, setItems] = useState([])
-    return (
-        <div className='container mt-10'>
-            <Row>
-                <Col xl={12}>
-                    <div className="card mb-4">
-                        <div className="card-body">
-                            <div className="card-header">
-                                <h5 className="card-title">Lista de Ventas</h5>
-                            </div>
+  const [items, setItems] = useState([]);
+  return (
+    <div className="container mt-10">
+      <Row>
+        <Col xl={12}>
+          <div className="card mb-4">
+            <div className="card-body">
+              <div className="card-header">
+                <h5 className="card-title">Lista de Ventas</h5>
+              </div>
 
-                            <Table responsive hover bordered className="m-0 text-center" >
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Fecha</th>
-                                        <th>IdVenta</th>
-                                        <th className="text-center">Detalle</th>
-                                        <th className="text-center">Valor</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {items && items.map((item, index) => {
-                                        return (
-                                            <tr key={index}>
-                                                <td className="text-left">{index + 1}</td>
-                                                <td className="text-left">{item.Fecha}</td>
-                                                <td className="text-left">{item.IdVenta}</td>
-                                                <td className="text-left">{item.Detalle}</td>
-                                                <td className="text-left">{item.Valor}</td>
-                                                <td>
-                                                    <ButtonGroup size="sm" aria-label="Basic example">
-                                                        <Button onClick={() => console.log('holaa')} variant="outline-secondary"><Icon.Edit3 className="icon wh-15" /></Button>
-                                                        <Button variant="outline-secondary" onClick={() => console.log('hola')}><Icon.Trash2 className="icon wh-15" /></Button>
-                                                    </ButtonGroup>
-                                                </td>
-                                            </tr>
-                                        )
-                                    })}
-                                </tbody>
-                            </Table>
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-        </div>
-    );
-}
+              <Table responsive hover bordered className="m-0 text-center">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Fecha</th>
+                    <th>IdVenta</th>
+                    <th className="text-center">Nombre</th>
+                    <th className="text-center">Descripcion</th>
+                    <th className="text-center">Precio</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {productsLista &&
+                    productsLista.map((item, index) => {
+                      return (
+                        <tr key={index}>
+                          <td className="text-left">{index + 1}</td>
+                          <td className="text-left">{item.fecha}</td>
+                          <td className="text-left">{item.idVenta}</td>
+                          <td className="text-left">{item.titulo}</td>
+                          <td className="text-left">{item.descripcion}</td>
+                          <td className="text-left">{item.precio}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </Table>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default compose(
-    withRouter
-)(Sales);
+export default compose(withRouter)(Sales);
