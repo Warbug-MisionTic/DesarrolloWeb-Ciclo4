79bb8e0e189e6148d5ab55cbbc80d33bc02ef38d
juandijez <juandijez@gmail.com>
2022-11-04 00:24:34 -0500

    Validaciones agregar productos

--- front-react/src/pages/Admin/add.jsx
+++ front-react/src/pages/Admin/add.jsx
@@ -2,37 +2,83 @@ import { withRouter } from "../../router/withRouter";
 import { compose } from "recompose";
 import React, {useState} from "react";
 import Form from 'react-bootstrap/Form';
+import { useForm } from "react-hook-form";
 
 const Add = () => {
-   
+  const { register, errors, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="ContenedorModPadre">
-     
-
       <div className="ContenedorMod">
         <h3>Agregar Productos </h3>
-        
+
         <div className="contenedor-main">
           <div className="contenedor-product">
-            <img className="imagenProducto"  />
-            <Form.Group controlId="formFile" className="mb-3" >
-                <Form.Label>Cambiar imagen</Form.Label>
-                <Form.Control type="file" />
+            <img className="imagenProducto" />
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label>Cambiar imagen</Form.Label>
+              <Form.Control type="file" />
             </Form.Group>
-            
           </div>
 
-          <div className="formulario">
-            <p className="labelMod">Nombre del producto</p>{" "}
-            <input type="text" className="fieldProduct"  /> <br />
-            <p className="labelMod">Descripción </p>{" "}
-            <input type="text" className="fieldProduct"  /> <br />
-            <p className="labelMod">Precio</p>{" "}
-            <input type="text" className="fieldProduct" />
-            <br />
-            <button className="botoncambiarimg"> Guardar cambios </button>
-          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              name="nombreP"
+              className="form-control my-2"
+              placeholder="Ingrese un nombre de producto"
+              {...register("nombreP", {
+                required: true,
+                maxLength: 20,
+                message: "Campo requerido", 
+              })}
+            />
 
+            <textarea
+              type="text"
+              name="descripcion"
+              className="form-control my-2"
+              placeholder="Ingrese la descripción del producto"
+              {...register("descripcion", {
+                required: true,
+                maxLength: 30,
+                message: "Campo requerido",
+              })}
+            />
+
+            <input
+              type="number"
+              name="precio"
+              className="form-control my-2"
+              placeholder="Ingrese el valor del producto"
+              {...register("precio", {
+                required: true,
+                maxLength: 8,
+                message: "Campo requerido",               })}
+            />
+
+            <input
+              type="number"
+              name="cantidad"
+              className="form-control my-2"
+              placeholder="Ingrese cantidad"
+              {...register("cantidad", {
+                required: true,
+                maxLength: 3,
+                message: "Campo requerido",
+              })}
+            />
+
+            <span className="text-danger text-small d-block mb-2">
+              {errors?.titulo?.message}
+            </span>
+
+            <button className="btn btn-primary">Guardar Cambios</button>
+          </form>
         </div>
       </div>
       {/* <Outlet /> */}
